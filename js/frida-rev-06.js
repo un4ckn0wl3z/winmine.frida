34a1f0ccbae9c7ad5efef8e53ab413101db4510b
Anuwat <admin@unknownclub.net>
2023-03-01 23:19:28 +0700

    feat: auto mark flag on mines

---
+++ js/frida-rev-06.js
@@ -0,0 +1,49 @@
+"use strict";
+class FridaRev06 {
+    constructor() {
+        this.module_name_winmine = "winmine.exe";
+        this.height = 0;
+        this.width = 0;
+        this.mines = 0;
+        this.entry = ptr(0);
+        console.log("=============================", new Date().toString(), "=============================");
+        console.log("Frida.version", Frida.version);
+        console.log("Frida.heapSize", Frida.heapSize);
+        console.log("Process.id", Process.id);
+        console.log("Process.arch", Process.arch);
+        console.log("Process.codeSigningPolicy", Process.codeSigningPolicy);
+        this.module_winmine = Process.getModuleByName(this.module_name_winmine);
+    }
+    load_board_info() {
+        this.height = this.module_winmine.base.add(0x5338).readU32();
+        this.width = this.module_winmine.base.add(0x5334).readU32();
+        this.mines = this.module_winmine.base.add(0x5330).readU32();
+        this.entry = this.module_winmine.base.add(0x5340);
+    }
+    board_info() {
+        this.board_mark();
+    }
+    board_mark(modify = false) {
+        this.load_board_info();
+        for (let i = 0; i < this.height + 2; i++) {
+            let data = [];
+            for (let j = 0; j < this.width + 2; j++) {
+                const bytes_data = this.entry.add(j + 0x20 * i).readU8();
+                if (modify) {
+                    if (bytes_data == 0x8F) {
+                        this.entry.add(j + 0x20 * i).writeU8(0x8E);
+                    }
+                }
+                else {
+                    data.push(bytes_data.toString(16).padStart(2, '0').toLocaleUpperCase());
+                }
+            }
+            if (!modify) {
+                console.log(data.join(" "));
+            }
+        }
+    }
+}
+let fridaRev06 = new FridaRev06();
+fridaRev06.board_mark(true);
+fridaRev06.board_info();
