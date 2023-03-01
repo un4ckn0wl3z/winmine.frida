1e03d4638bc35e54f8b7b2b5921042e7fedc503b
Anuwat <admin@unknownclub.net>
2023-03-01 22:57:46 +0700

    init hack

---
+++ frida-rev-04.ts
@@ -0,0 +1,15 @@
+function frida_rev_04(){
+    console.log("=============================", new Date().toString(), "=============================");
+    console.log("Frida.version", Frida.version);
+    console.log("Frida.heapSize", Frida.heapSize);
+    console.log("Process.id", Process.id);
+    console.log("Process.arch", Process.arch);
+    console.log("Process.codeSigningPolicy", Process.codeSigningPolicy);
+    let module = Process.enumerateModules();
+    for (const iterator of module) {
+        console.log(iterator.base, iterator.name, iterator.size);
+    }
+}
+
+frida_rev_04();
+console.log("OK")
